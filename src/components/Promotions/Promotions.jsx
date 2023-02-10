18fcd32c14f8282fe28d19074f38a0ece5f6da1e
mzastsenski <koljan_z@yahoo.de>
2023-02-10 11:01:41 +0100

    promotions changed

--- src/components/Promotions/Promotions.jsx
+++ src/components/Promotions/Promotions.jsx
@@ -1,18 +1,28 @@
 import s from "./Promotions.module.scss";
 import ProductCard from "../ProductCard/ProductCard";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 export default function Categories() {
   const products = useSelector((state) => state.data.products);
-  const num = Math.floor(Math.random() * (products.length - 5));
+  const [promotions, setPromotions] = useState([]);
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    setPromotions(
+      products.filter((e) => e.discont_price !== e.price && !e.hide)
+    );
+    setNum(Math.floor(Math.random() * (promotions.length - 3)));
+  }, [products, promotions.length]);
+
   return (
     <section className={s.promotions}>
       <h1>Promotions</h1>
-      {products.length > 0 && (
+      {promotions.length > 0 && (
         <div className={s.promotions_container}>
-          <ProductCard {...products[num]} />
-          <ProductCard {...products[num + 2]} />
-          <ProductCard {...products[num + 5]} />
+          <ProductCard {...promotions[num]} />
+          <ProductCard {...promotions[num + 2]} />
+          <ProductCard {...promotions[num + 3]} />
         </div>
       )}
     </section>
