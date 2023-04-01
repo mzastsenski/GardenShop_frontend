44773bd1ad7590d1967efe65eaf54a260d2f67c9
mzastsenski <koljan_z@yahoo.de>
2023-04-01 22:22:58 +0200

    sale component changed

--- src/components/Sale/Sale.jsx
+++ src/components/Sale/Sale.jsx
@@ -4,17 +4,21 @@ import { HashLink } from "react-router-hash-link";
 export default function Categories() {
   return (
     <section id="sale" className={s.sale}>
-      <h1>Sale</h1>
-      <h2>New season</h2>
-      <div>
-        <HashLink smooth to="/#promotions">
-          <button> Sale </button>
-        </HashLink>
-        <HashLink smooth to="/#promotions">
-          <button>Learn more</button>
-        </HashLink>
+      <div className={s.left}>
+        <h1>Sale</h1>
+        <h2>New season</h2>
+        <div>
+          <HashLink smooth to="/#promotions">
+            <button> Sale </button>
+          </HashLink>
+          <HashLink smooth to="/#promotions">
+            <button>Learn more</button>
+          </HashLink>
+        </div>
       </div>
-      <img src="images/sale_image.png" alt="" />
+      <div className={s.img_container}>
+        <img src="images/sale_image2.png" alt="" />
+      </div>
     </section>
   );
 }
